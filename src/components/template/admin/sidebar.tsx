3c1a21d2557cceea8ef0c419bfecff05c50a8622
MA ZAHA <mazaha@192.168.1.6>
2023-12-10 09:46:21 +0700

    add menu sidebar admin

--- src/components/template/admin/sidebar.tsx
+++ src/components/template/admin/sidebar.tsx
@@ -19,7 +19,22 @@ const Sidebar = () => {
                         <span>Dashboard</span>
                     </Link>
                 </li>
+
                 <li className="nav-heading">Master Data</li>
+
+                <li className="nav-item" key={'1'}>
+                    <Link
+                        href={"/panel/santri_baru/tambah"}
+                        className={`nav-link ${show === 'tsb' ? '' : 'collapsed'}`}
+                        onClick={() => {
+                            setActive(null);
+                            setShow('tsb');
+                        }}>
+                        <i className="bi bi-person-plus"></i>
+                        <span>Tambah Santri Baru</span>
+                    </Link>
+                </li>
+
                 <li className="nav-item">
                     <Link
                         className={`nav-link ${show === 'pendaftar' ? '' : 'collapsed'}`}
@@ -35,7 +50,7 @@ const Sidebar = () => {
                                 setShow('pendaftar')
                             }
                         }}>
-                        <i className="bi bi-person" style={{textDecoration: "none"}}></i>
+                        <i className="bi bi-people" style={{textDecoration: "none"}}></i>
                         <span>Pendaftar</span>
                         <i className={`bi bi-chevron-down ms-auto`}></i>
                     </Link>
@@ -52,6 +67,164 @@ const Sidebar = () => {
                             >
                                 <Icon icon="gg:check-o" color="green" style={{marginRight: 5}}/>
                                 <span>Data Santri Baru</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/panel/santri_baru"}
+                                className={active === 'perlu_verifikasi' ? 'active' : ''}
+                                data-bs-target="#components-nav"
+                                data-bs-toggle="collapse"
+                                style={{textDecoration: "none"}}
+                            >
+                                <Icon icon="gg:check-o" color="green" style={{marginRight: 5}}/>
+                                <span>Perlu Verifikasi</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/panel/santri_baru"}
+                                className={active === 'registrasi' ? 'active' : ''}
+                                data-bs-target="#components-nav"
+                                data-bs-toggle="collapse"
+                                style={{textDecoration: "none"}}
+                            >
+                                <Icon icon="gg:check-o" color="green" style={{marginRight: 5}}/>
+                                <span>Registrasi</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </li>
+
+                <li className="nav-item">
+                    <Link
+                        className={`nav-link ${show === 'pengaturan' ? '' : 'collapsed'}`}
+                        data-bs-target="#components-nav"
+                        data-bs-toggle="collapse"
+                        style={{textDecoration: "none"}}
+                        aria-expanded={show === 'pengaturan' ? true : false}
+                        href="#"
+                        onClick={() => {
+                            if (show === 'pengaturan') {
+                                setShow('')
+                            } else {
+                                setShow('pengaturan')
+                            }
+                        }}>
+                        <i className="bi bi-gear" style={{textDecoration: "none"}}></i>
+                        <span>Pengaturan</span>
+                        <i className={`bi bi-chevron-down ms-auto`}></i>
+                    </Link>
+                    <ul id="components-nav"
+                        className={`nav-content ${show === 'pengaturan' ? 'collapsed' : 'collapse'}`}
+                        data-bs-parent="#sidebar-nav">
+                        <li>
+                            <Link
+                                href={"/panel/santri_baru"}
+                                className={active === 'pengaturan_aplikasi' ? 'active' : ''}
+                                data-bs-target="#components-nav"
+                                data-bs-toggle="collapse"
+                                style={{textDecoration: "none"}}
+                            >
+                                <Icon icon="gg:check-o" color="green" style={{marginRight: 5}}/>
+                                <span>Pengaturan Aplikasi</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/panel/santri_baru"}
+                                className={active === 'panitia' ? 'active' : ''}
+                                data-bs-target="#components-nav"
+                                data-bs-toggle="collapse"
+                                style={{textDecoration: "none"}}
+                            >
+                                <Icon icon="gg:check-o" color="green" style={{marginRight: 5}}/>
+                                <span>Panitia</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/panel/santri_baru"}
+                                className={active === 'brosur' ? 'active' : ''}
+                                data-bs-target="#components-nav"
+                                data-bs-toggle="collapse"
+                                style={{textDecoration: "none"}}
+                            >
+                                <Icon icon="gg:check-o" color="green" style={{marginRight: 5}}/>
+                                <span>Brosur</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/panel/santri_baru"}
+                                className={active === 'admin' ? 'active' : ''}
+                                data-bs-target="#components-nav"
+                                data-bs-toggle="collapse"
+                                style={{textDecoration: "none"}}
+                            >
+                                <Icon icon="gg:check-o" color="green" style={{marginRight: 5}}/>
+                                <span>Admin</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </li>
+
+                <li className="nav-item">
+                    <Link
+                        className={`nav-link ${show === 'laporan' ? '' : 'collapsed'}`}
+                        data-bs-target="#components-nav"
+                        data-bs-toggle="collapse"
+                        style={{textDecoration: "none"}}
+                        aria-expanded={show === 'laporan' ? true : false}
+                        href="#"
+                        onClick={() => {
+                            if (show === 'laporan') {
+                                setShow('')
+                            } else {
+                                setShow('laporan')
+                            }
+                        }}>
+                        <i className="bi bi-list-task" style={{textDecoration: "none"}}></i>
+                        <span>Laporan</span>
+                        <i className={`bi bi-chevron-down ms-auto`}></i>
+                    </Link>
+                    <ul id="components-nav"
+                        className={`nav-content ${show === 'laporan' ? 'collapsed' : 'collapse'}`}
+                        data-bs-parent="#sidebar-nav">
+                        <li>
+                            <Link
+                                href={"/panel/santri_baru"}
+                                className={active === 'laporan_semua_data' ? 'active' : ''}
+                                data-bs-target="#components-nav"
+                                data-bs-toggle="collapse"
+                                style={{textDecoration: "none"}}
+                            >
+                                <Icon icon="gg:check-o" color="green" style={{marginRight: 5}}/>
+                                <span>Semua Data</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/panel/santri_baru"}
+                                className={active === 'data_registrasi' ? 'active' : ''}
+                                data-bs-target="#components-nav"
+                                data-bs-toggle="collapse"
+                                style={{textDecoration: "none"}}
+                            >
+                                <Icon icon="gg:check-o" color="green" style={{marginRight: 5}}/>
+                                <span>Data Registrasi</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/panel/santri_baru"}
+                                className={active === 'kartu_tes' ? 'active' : ''}
+                                data-bs-target="#components-nav"
+                                data-bs-toggle="collapse"
+                                style={{textDecoration: "none"}}
+                            >
+                                <Icon icon="gg:check-o" color="green" style={{marginRight: 5}}/>
+                                <span>Cetak Kartu Tes</span>
                             </Link>
                         </li>
                     </ul>
---
+++ src/pages/panel/santri_baru/tambah.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PanelContent from "@/components/panelContent";
+import Template from "@/components/template/admin/template";
+
+const Tambah = () => {
+    return (
+        <Template>
+            <PanelContent title={"Tambah Santri Baru"}>
+                <h1>ini data santri baru</h1>
+            </PanelContent>
+        </Template>
+    );
+};
+
+export default Tambah;
