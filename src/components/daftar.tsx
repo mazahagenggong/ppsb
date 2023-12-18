fab89ddb5f4d7eea0ffe36ddcc6b5c1f8f571b4e
Abunaum <abunaum@hotmail.com>
2023-12-18 09:52:41 +0700

    refixing bukti pendaftaran

--- src/components/daftar.tsx
+++ src/components/daftar.tsx
@@ -507,8 +507,7 @@ const DaftarForm: React.FC<DaftarFormProps> = (props: DaftarFormProps) => {
                                     try {
                                         showWaitLoading("Membuat bukti pendaftaran.")
                                         const {data} = (await axios.post("/api/cek/siswa", {id: datalogin.id})).data;
-                                        const url = `${app_url}/downloads/formulir/${datalogin.id}`;
-                                        await createPDF(data, `https://quickchart.io/qr?text=${url}&dark=018417&margin=2&size=300&centerImageUrl=https%3A%2F%2Fi.ibb.co%2Fb2hZf16%2Fmalogo.png`);
+                                        await createPDF(data, datalogin.id);
                                         await LoadingTimer("Bukti pendaftaran berhasil di buat.", "success", 3000);
                                     } catch (e) {
                                         await LoadingTimer("Bukti pendaftaran gagal di buat.", "error", 3000);
