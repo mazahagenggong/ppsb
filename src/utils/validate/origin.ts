5c51d885256c3a517f2503669717a140f7312f2d
Abunaum <abunaum@hotmail.com>
2024-01-08 09:10:26 +0700

    beta origin

--- src/utils/validate/origin.ts
+++ src/utils/validate/origin.ts
@@ -1,7 +1,7 @@
 import type {NextApiRequest} from "next";
 
 export const Origin = async (req: NextApiRequest) => {
-    const allowedDomains = ['http://localhost:3000','https://ppsb.mazainulhasan1.sch.id','https://ppsb.vercel.app'];
+    const allowedDomains = ['http://localhost:3000','https://ppsb.mazainulhasan1.sch.id','https://ppsb.vercel.app','https://beta-ppsb.vercel.app'];
     const clientOrigin = req.headers.origin;
     if (!clientOrigin || !allowedDomains.includes(clientOrigin)) {
         return {
